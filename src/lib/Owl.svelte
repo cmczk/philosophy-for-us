bd22ae80b181d82e26e660b67bbad047bcb09600
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-03-15 00:24:33 +0300

    Create the Owl

---
+++ src/lib/Owl.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center">
+  <pre class="owl mt-10 text-[44px] font-bold">
+  '.___.'  
+___(*v*)___
+`'-\|_|/-'`
+    ^ ^
+  </pre>
+</div>
--- src/routes/about/about-us/+page.svelte
+++ src/routes/about/about-us/+page.svelte
@@ -1,1 +1,12 @@
-<h1>About</h1>
+<script>
+  import Owl from "$lib/Owl.svelte";
+</script>
+
+<h1 class="text-center text-4xl">Кто мы?</h1>
+<div class="about-us-block">
+  <p>
+    <b>Philosophy of Us</b> -- это проект, возникший из желания показать, что философия может быть понятной
+    и интересной, если о ней рассказывают действительно увлечённые люди.
+  </p>
+</div>
+<Owl />
