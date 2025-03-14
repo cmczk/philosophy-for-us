b060ae11f5dba71b56e5b3363716e5497e334950
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-03-14 20:14:18 +0300

    Create dropdown menu

---
+++ src/lib/DropdownMenu.svelte
@@ -0,0 +1,58 @@
+<script>
+  let isMenuOpen = $state(false);
+</script>
+
+<button
+  class="navbar-menu-button flex cursor-pointer items-center"
+  type="button"
+  onclick={() => (isMenuOpen = !isMenuOpen)}
+  >О проекте
+  <svg
+    class="pt-0.5 transition-all duration-500 {isMenuOpen ? '-rotate-180' : 'rotate-0'}"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
+      fill="#000000"
+    />
+  </svg>
+</button>
+
+<ul class="h-0 w-0 overflow-hidden {isMenuOpen ? 'about-menu bg-slate-100' : ''}">
+  <li><a class="about-menu-link hover:bg-slate-400 hover:text-white" href="#top">Кто мы?</a></li>
+  <li>
+    <a class="about-menu-link hover:bg-slate-400 hover:text-white" href="#top"
+      >Как присоединиться?</a
+    >
+  </li>
+  <li><a class="about-menu-link hover:bg-slate-400 hover:text-white" href="#top">Правила</a></li>
+</ul>
+
+<style>
+  .navbar-menu-button {
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+
+  .about-menu {
+    position: absolute;
+    top: 100%;
+    right: 0%;
+    display: flex;
+    flex-direction: column;
+    width: 220px;
+    height: auto;
+  }
+
+  .about-menu-link {
+    display: block;
+    white-space: nowrap;
+    padding: 20px;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import "../app.css";
+  import DropdownMenu from "$lib/DropdownMenu.svelte";
   let { children } = $props();
 
   let isMenuOpen = $state(false);
@@ -14,25 +15,8 @@
         <li><a class="navbar-menu-link" href="#top">Переводы</a></li>
         <li><a class="navbar-menu-link" href="#top">Конспекты</a></li>
         <li><a class="navbar-menu-link" href="/flashcards">Карточки</a></li>
-        <li>
-          <button class="navbar-menu-button flex cursor-pointer items-center" type="button"
-            >О проекте
-            <svg
-              class="pt-0.5"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
-                fill="#000000"
-              />
-            </svg>
-          </button>
+        <li class="relative">
+          <DropdownMenu />
         </li>
       </ul>
       <ul class="header-navbar-contacts flex items-center justify-between gap-x-5">
@@ -70,8 +54,7 @@
     margin: 0 auto;
   }
 
-  .navbar-menu-link,
-  .navbar-menu-button {
+  .navbar-menu-link {
     text-transform: uppercase;
     font-weight: 700;
   }
