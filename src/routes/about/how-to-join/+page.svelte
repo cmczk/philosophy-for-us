c16d7ea8a571cc9c231b3a9995776bfa5cac3642
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-03-18 22:51:11 +0300

    Add about content wip

--- src/routes/about/how-to-join/+page.svelte
+++ src/routes/about/how-to-join/+page.svelte
@@ -1,1 +1,19 @@
-<h1>How to join</h1>
+<h1>Как присоединиться?</h1>
+
+<section>
+  <div class="how-to-join-section-title">
+    <h2>Как создаются тексты на платформе?</h2>
+  </div>
+  <div class="how-to-join-section-body">
+    <p></p>
+  </div>
+</section>
+
+<section>
+  <div class="how-to-join-section-title">
+    <h2>Какие тексты можно присылать?</h2>
+  </div>
+  <div class="how-to-join-section-body">
+    <p></p>
+  </div>
+</section>
--- src/routes/about/rules/+page.svelte
+++ src/routes/about/rules/+page.svelte
@@ -1,16 +1,53 @@
 <h1>Правила платформы Philosophy for Us</h1>
 
-<h2>Как писать материалы?</h2>
-<ol>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-</ol>
+<section class="how-to-write-section">
+  <div class="how-to-write-section-title">
+    <h2>Как писать материалы?</h2>
+  </div>
 
-<h2>Лицензии</h2>
-<ol>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-</ol>
+  <div class="how-to-write-section-body">
+    <ol class="rules-list">
+      <li class="rules-item">
+        Материал, который вы присылаете, должен быть написан вами самостоятельно. Желательно, чтобы
+        темы, на которую вы пишете, мы для вас интересна и личностно близка: вы делитесь этим с
+        другими людьми, которых может заинтересовать та же идея, мысль, цитата, книга,
+        биографический факт и т. д.
+      </li>
+      <li class="rules-item">
+        <ol class="rules-sublist">
+          <li class="rules-sublist-item">
+            Не используйте искусственный интеллект для генерации текстов ваших материалов. Мы не
+            против AI и развития технологий, но смысл Philosophy for Us - в создании сообщества
+            людей, интересующихся философией и готовых делиться своими знаниями с другими. Поэтому
+            для нас важно, чтобы материал писали именно вы и именно для сообщества. Мы рассчитываем,
+            что, если человек захочет пообщаться с генеративной нейросейтью о какой-либо философской
+            концепции, то он сможет сделать это и без нашей плаформы. Здесь же мы создаём для
+            каждого интересующегося пространство для высказывания и знакомства с философией через
+            опыт других людей.
+          </li>
+          <li class="rules-sublist-item">
+            Вы можете использовать искусственный интеллект для обсуждения темы, на которую вы пишете
+            материал, поиска дополнительных фактов, вычитки итогового текста для улучшения его
+            стиля, но не для генерации самого текста.
+          </li>
+        </ol>
+      </li>
+      <li class="rules-item">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+      <li class="rules-item">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li class="rules-item">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+    </ol>
+  </div>
+</section>
+
+<section class="license-section">
+  <div class="license-section-title">
+    <h2>Лицензии</h2>
+  </div>
+
+  <div class="license-section-body">
+    <ol class="license-list">
+      <li class="license-item">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li class="license-item">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+    </ol>
+  </div>
+</section>
